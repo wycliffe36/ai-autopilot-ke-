f4de5820fba327e762365d4bcc35e28a19274306
wycliffe36 <73503275+wycliffe36@users.noreply.github.com>
2026-08-08 19:58:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ genai.configure(api_key=GEMINI_KEY)
 prompt = st.text_input("Ask me anything:")
 
 if st.button("Send"):
-    model = genai.GenerativeModel("gemini-pro")
+    model = genai.GenerativeModel("gemini-1.0-pro")
     response = model.generate_content(prompt)
     st.write(response.text)
