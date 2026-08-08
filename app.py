3a45f749b99bef7bfaf72a51b48c8da0d0bb1db4
wycliffe36 <73503275+wycliffe36@users.noreply.github.com>
2026-08-08 21:03:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-    import streamlit as st
 
-    from google import genai
-    import os
+import streamlit as st
+from google import genai
+import os
 
-    st.title("🚀 AI Autopilot KE")
-    client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
+st.title("🚀 AI Autopilot KE")
+client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
 
-    if prompt := st.chat_input("Ask me anything"):
-        res = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
-        st.write(res.text)
-
-
+if prompt := st.chat_input("Ask me anything"):
+    res = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    st.write(res.text)
